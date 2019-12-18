fd8dda53fcf113c2c04f08cd4115b2d98ecc0276
jenniferlai43 <jenniferlai43@gmail.com>
2019-12-18 15:17:04 -0800

    Add middle ratings for a 3

--- src/client/components/AppReview/RatingSelect.js
+++ src/client/components/AppReview/RatingSelect.js
@@ -11,7 +11,9 @@ class RatingSelect extends React.Component {
 			  <option value=''>/</option>
 			  <option value="1">1</option>
 			  <option value="2">2</option>
-			  <option value="3">3</option>
+			  <option value="3.25">3L</option>
+			  <option value="3.5">3M</option>
+			  <option value="3.75">3H</option>
 			  <option value="4">4</option>
 			  <option value="5">5</option>
 			</select>
